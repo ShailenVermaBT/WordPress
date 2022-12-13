1d6dda8e05e88442ec7c3925485493cfbb6168cf
Ashish <ashishbairwa232@gmail.com>
2022-12-13 14:56:04 +0530

    7th commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo apt-get install php7.4 -y
 sudo apt install apache2 -y
 sudo chown ubuntu /etc/apache2/ports.conf
 sudo chmod +w /etc/apache2/ports.conf
-sudo sed -i 's/80/88/g' /etc/apache2/ports.conf
-sudo apt install -y mariadb-server php
+sudo sed -i 's/80/88/g' /etc/apache2/ports.
+sudo systemctl restart apache2
+sudo apt install -y mariadb-server php libapache2-mod-php
+sudo apt update
+sudo apt install php-fpm -y
+sudo apt-get install php7.4 -y
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 systemctl start mariadb.service
 systemctl start apache2.service
-systemctl start php-fpm.service
+systemctl start php7.4-fpm.service
